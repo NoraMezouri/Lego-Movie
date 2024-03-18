b9ea86b292b385233a30f7b4431323fdf1b54a63
NoraMezouri <156891253+NoraMezouri@users.noreply.github.com>
2024-03-18 09:19:02 +0100

    fdsfds

--- spline.js
+++
@@ -1,6 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/ll9sY7zZywJAeh9B/scene.splinecode');
-
